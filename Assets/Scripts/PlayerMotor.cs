c3aa9a4940a3ae65947d3f856bf9d000ba572736
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-02-05 19:06:55 -0800

    Add basic player and camera movement

---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 5.0f;
+    private Vector3 moveVector;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        moveVector = Vector3.zero;
+
+        // X (left - right)
+        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+
+        // Y (up - down)
+
+        // Z (forward - backward)
+        moveVector.z =  Input.GetAxisRaw("Vertical") * speed;
+
+        controller.Move(moveVector * speed * Time.deltaTime);
+    }
+}
