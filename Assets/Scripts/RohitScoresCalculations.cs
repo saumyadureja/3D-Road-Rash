73fdfe5260a7b618ae0cfd8a69809b89e9aff158
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-02-10 22:17:20 -0800

    Score functionality added

---
+++ Assets/Scripts/RohitScoresCalculations.cs
@@ -0,0 +1,22 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RohitScoresCalculations : MonoBehaviour
+{
+    private float score = 0.0f;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score += Time.deltaTime;
+        scoreText.text = ((int)score).ToString();
+    }
+}
