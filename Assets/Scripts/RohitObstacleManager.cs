59be60e3a05c589f9035df56e43583247507e752
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-02-10 19:20:56 -0800

    Obstacle prefabs created

---
+++ Assets/Scripts/RohitObstacleManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RohitObstacleManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    private Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
