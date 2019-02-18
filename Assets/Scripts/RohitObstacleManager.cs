30d5e3631f9b3db549ca90ae43f43912152baac9
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-02-17 23:12:21 -0800

    Obstacle manager updated with random instances according to player position

--- Assets/Scripts/RohitObstacleManager.cs
+++ Assets/Scripts/RohitObstacleManager.cs
@@ -7,30 +7,46 @@ public class RohitObstacleManager : MonoBehaviour
     public GameObject[] obstaclePrefabs;
     private Transform playerTransform;
     private readonly int noOfObstacles = 10;
+    private float obstacleCreatedTillZ;
 
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-
-        for(int i = 0; i < noOfObstacles; i++)
-        {
-            spawnObstacles(i);
-        }
+        obstacleCreatedTillZ = playerTransform.position.z + 10.0f;
+        //for (int i = 0; i < noOfObstacles; i++)
+        //{
+           // spawnObstacles(i);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (playerTransform.position.z + 50.0f > obstacleCreatedTillZ)
+        {
+            spawnObstaclesOnZ(obstacleCreatedTillZ);
+            obstacleCreatedTillZ += 50;
+        }
+    }
+    private void spawnObstaclesOnZ(float start) 
+    {
+        for (float  i = start; i < start + 50.0f; i += 10 )
+        {
+            Vector3 position = new Vector3(Random.Range(-4.0f, 4.0f), 1, i);
+            GameObject go;
+            go = Instantiate(obstaclePrefabs[Random.Range(0, 3)]);
+            go.transform.SetParent(GameObject.FindGameObjectWithTag("ObstacleManagerTag").transform);
+            go.transform.position = position;
+        }
         
     }
-
-    private void spawnObstacles(int index  = -1)
-    {
-        Vector3 position = new Vector3(Random.Range(-4.0f, 4.0f), 1, Random.Range((10.0f * index), (10.0f * (index+1))));
-        GameObject go;
-        go = Instantiate(obstaclePrefabs[Random.Range(0, 3)]);
-        go.transform.SetParent(GameObject.FindGameObjectWithTag("ObstacleManagerTag").transform);
-        go.transform.position = position; 
-    }
+    //private void spawnObstacles(int index  = -1)
+    //{
+    //    Vector3 position = new Vector3(Random.Range(-4.0f, 4.0f), 1, Random.Range((10.0f * index), (10.0f * (index+1))));
+    //    GameObject go;
+    //    go = Instantiate(obstaclePrefabs[Random.Range(0, 3)]);
+    //    go.transform.SetParent(GameObject.FindGameObjectWithTag("ObstacleManagerTag").transform);
+    //    go.transform.position = position; 
+    //}
 }
