05faa63b5d4963997cde30ee9f6b6ebfb63203ab
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-03-23 22:34:12 -0700

    Two progress bars for intuitive radiation distance

--- Assets/Scripts/RohitRadiation.cs
+++ Assets/Scripts/RohitRadiation.cs
@@ -9,12 +9,14 @@ public class RohitRadiation : MonoBehaviour
     private Transform playerTransform;
     private readonly float offset = 200.0f;
     private readonly float radiationSpeed = 20.0f;
-    public Image progressBar;
+    public Image radiationPositiveProgress;
+    public Image radiationNegativeProgress;
     // Start is called before the fist frame update
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
-
+        radiationNegativeProgress.fillAmount = 0.0f;
+        radiationPositiveProgress.fillAmount = 0.0f;
         radiationLocation = playerTransform.position.z - offset;
     }
 
@@ -26,7 +28,20 @@ public class RohitRadiation : MonoBehaviour
         // Radiation distance logger
         float distance;
         distance = playerTransform.position.z - radiationLocation;
-        progressBar.fillAmount = (distance / 200.0f);
+
+        if(distance < 0)
+        {
+            // negative
+            radiationNegativeProgress.fillAmount = (distance*(-1) / 200.0f);
+            radiationPositiveProgress.fillAmount = 0.0f;
+        }
+        else
+        {
+            // positive
+            radiationPositiveProgress.fillAmount = (distance / 200.0f);
+            radiationNegativeProgress.fillAmount = 0.0f;
+        }
+       
         //Debug.Log("Distance from radiation is: " + distance + " Fill Amount: " + progressBar.fillAmount);
     }
 }
