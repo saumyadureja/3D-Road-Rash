5ce44b2e172a3b6b800b90d6d8c122a80f269062
Rohit Sahasrabuddhe <rohitbhalchandras@gmail.com>
2019-03-23 22:54:34 -0700

    Bug Fix: Camera movement accelerometer

--- Assets/Scripts/AccelerometerInput.cs
+++ Assets/Scripts/AccelerometerInput.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class AccelerometerInput : MonoBehaviour
 {
+    private float transition = 0.0f;
+    private float animationDuration = 3.0f;
+
     void Update()
     {
         if (GetComponent<RohitMovePlayer>().isDead)
         {
             Destroy(this);
         }
-        transform.Translate(Input.acceleration.x, 0, 0);
+
+        if (transition > 1.0f)
+        {
+            transform.Translate(Input.acceleration.x, 0, 0);
+        }
+        else
+        {
+            //Animation at the start of the game
+            transition += Time.deltaTime * 1 / animationDuration;
+        }
+        
     }
 }
