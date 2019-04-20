d30de502e602740f649d18de0da5d6af1959505d
saumyadureja <saumyadureja@gmail.com>
2019-04-19 23:44:51 -0700

    deleted extra assets, made prefabs for ramp

---
+++ Assets/moveRamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRamp : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+     private Vector3 pos1 = new Vector3(-4,0,0);
+     private Vector3 pos2 = new Vector3(4,0,0);
+     public float speed = 1.0f;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time*speed, 1.0f));
+    }
+}
