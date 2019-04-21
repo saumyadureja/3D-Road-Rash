c19f955ff02b14e1ce13f9cf2f439999ddfd4d33
Mythri-Gaddam <mgaddam@usc.edu>
2019-04-21 14:48:37 -0700

    Modified the positions script for fixed prefab lengths.

--- Assets/Resources/ReadPositions.cs
+++ Assets/Resources/ReadPositions.cs
@@ -7,6 +7,8 @@ public class ReadPositions : MonoBehaviour
 {
 
     //public Transform prefab;
+    private float tileLength = 10.0f;
+    private float add = 10.0f;
 
     void Start()
     {
@@ -17,11 +19,11 @@ public class ReadPositions : MonoBehaviour
     {
         StreamReader streamReader = new StreamReader("Assets/Resources/positions.csv");
         bool eof = false;
-       
+
         while (!eof)
         {
-            string data = streamReader.ReadLine ();
-            if(data ==null)
+            string data = streamReader.ReadLine();
+            if (data == null)
             {
                 eof = true;
                 break;
@@ -29,26 +31,31 @@ public class ReadPositions : MonoBehaviour
 
             }
             string[] data_values = data.Split(',');
-            for(int i = 0; i < data_values.Length; i++)
+            for (int i = 0; i < data_values.Length; i++)
             {
                 //Debug.Log("Values:" + i.ToString() + " " + data_values[i].ToString());
                 //Debug.Log(data_values[i].ToString());
-                
+
                 //Debug.Log(" value: "+data_values[i]);//4
                 if (data_values[i] == "TileNewL1")
                 {
-                    GameObject tileNewPrefab = Instantiate(Resources.Load("TileNewL1"), new Vector3(int.Parse(data_values[i+1]), int.Parse(data_values[i + 2]), int.Parse(data_values[i + 3])), Quaternion.identity) as GameObject;
+                    add += tileLength;
+                    GameObject tileNewPrefab = Instantiate(Resources.Load("TileNewL1"), new Vector3(0, 0, add), Quaternion.identity) as GameObject;
+
 
                 }
                 //GameObject tileInstances1 = Instantiate(Resources.Load("TileNew"), new Vector3(0, 0, 30), Quaternion.identity) as GameObject;
                 if (data_values[i] == "Tile_RampLeftL1")
                 {
-                    GameObject tileLeftPrefab = Instantiate(Resources.Load("Tile_RampLeftL1"), new Vector3(int.Parse(data_values[i + 1]), int.Parse(data_values[i + 2]), int.Parse(data_values[i + 3])), Quaternion.identity) as GameObject;
+                    add += tileLength;
+                    GameObject tileLeftPrefab = Instantiate(Resources.Load("Tile_RampLeftL1"), new Vector3(0, 0, add), Quaternion.identity) as GameObject;
+
 
                 }
                 if (data_values[i] == "Tile_RampRightL1")
                 {
-                    GameObject tileRightPrefab = Instantiate(Resources.Load("Tile_RampRightL1"), new Vector3(int.Parse(data_values[i + 1]), int.Parse(data_values[i + 2]), int.Parse(data_values[i + 3])), Quaternion.identity) as GameObject;
+                    add += tileLength;
+                    GameObject tileRightPrefab = Instantiate(Resources.Load("Tile_RampRightL1"), new Vector3(0, 0, add), Quaternion.identity) as GameObject;
 
                 }
 
