b009e0b1d05cab8092cddc6e6fec9fb2685d049d
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-04-25 15:04:23 -0700

    Ramp can now be slowed down

--- Assets/Scripts/moveRamp.cs
+++ Assets/Scripts/moveRamp.cs
@@ -8,12 +8,13 @@ public class MoveRamp : MonoBehaviour
 
     private Vector3 pos1;
     private Vector3 pos2; 
-    public float speed = 0.05f;
+    public float speed;
 
     private void Start()
     {       
         pos1 = new Vector3(-4, 3.53f, transform.position.z);
         pos2 = new Vector3(4, 3.53f, transform.position.z);
+        speed = 0.5f;
     }
 
     // Update is called once per frame
