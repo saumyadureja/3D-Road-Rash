507c72ceef477f9e1f3ca42a45d226f022e0ef8d
Siddhesh Karekar <siddhesh.r.karekar@gmail.com>
2019-04-27 18:56:39 -0700

    Player remains a ball

--- Assets/Scripts/RohitMovePlayer.cs
+++ Assets/Scripts/RohitMovePlayer.cs
@@ -8,9 +8,6 @@ using UnityEngine.UI;
 
 public class RohitMovePlayer : MonoBehaviour
 {
-    public GameObject cube;
-    public GameObject sphere;
-    public GameObject capsule;
     private CharacterController controller;
     public Text addValueText;
     private float speed = 10.0f;
@@ -21,8 +18,6 @@ public class RohitMovePlayer : MonoBehaviour
     private float gravity = 12.0f;
     public Boolean isDead = false;
     private float animationDuration = 3;
-    private Boolean isSphere = false;
-    private Boolean isCube = false;
     private CultureInfo ci= new CultureInfo("en-US");
     private Vector3 moveDirection = Vector3.zero;
     public float jumpSpeed = 8.0f;
@@ -31,32 +26,14 @@ public class RohitMovePlayer : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        capsule.SetActive(true);
-        sphere.SetActive(false);
-        cube.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-        if (isSphere)
-        {
-            sphere.SetActive(true);
-            cube.SetActive(false);
-            capsule.SetActive(false);
-        }
-        if (isCube)
-        {
-            sphere.SetActive(false);
-            cube.SetActive(true);
-            capsule.SetActive(false);
-        }
         if (isDead)
         {
             Debug.Log("MovePlayer: Is Dead");
-            //Destroy(this);
             return;
         }
         if (Time.time < animationDuration)
@@ -137,38 +114,6 @@ public class RohitMovePlayer : MonoBehaviour
                 Color color = hit.gameObject.GetComponent<MeshRenderer>().material.color;
                 color.a = 0.5f;
                 hit.gameObject.GetComponent<MeshRenderer>().material.color = color;
-
-                //Debug.Log("in health reduction if");
-                //Destroy(hit.gameObject);
-                isSphere = true;
-                isCube = false;
-
-            }
-            //else if (hit.gameObject.name == "Obstacle_Cube(Clone)")
-            //{
-            //    // Cube hit
-            //    GetComponent<RohitPlayerState>().SkatePowerUp();
-            //    Destroy(hit.gameObject);
-            //    isCube = true;
-            //    isSphere = false;
-            //}
-            //else if (hit.gameObject.name == "Obstacle_Cylinder(Clone)")
-            //{
-            //    // reduce the  health
-            //    Destroy(hit.gameObject);
-            //    GetComponent<RohitHealthCalculation>().ReduceHealth(30.0f);
-
-            //}
-            //else if (hit.gameObject.name.StartsWith("Obstacle_Health", true, ci))
-            //{
-            //    // reduce the  health
-            //    Destroy(hit.gameObject);
-            //    GetComponent<RohitHealthCalculation>().IncreaseHealth(40.0f);
-
-            //}
-            else
-            {
-                // Death();
             }
         }
 
@@ -178,6 +123,7 @@ public class RohitMovePlayer : MonoBehaviour
     {
         isDead = true;
         GetComponent<RohitScoresCalculations>().OnDeathScoreStops();
+        // Destroy(this);
     }
 
     public void IncreaseSpeedByVal(int difficultLevel)
--- Assets/Scripts/Rotate360.cs
+++ Assets/Scripts/Rotate360.cs
@@ -13,6 +13,6 @@ public class Rotate360 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(0, 0, 100 * Time.deltaTime);
+        transform.Rotate(0, 170 * Time.deltaTime, 0);
     }
 }
